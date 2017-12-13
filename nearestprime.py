cbfc5b3248540cd670748bb842850acbd2b25fe8
mblaul <32026169+mblaul@users.noreply.github.com>
2017-12-12 22:46:57 -0500

    Working 100%

--- nearestprime.py
+++ nearestprime.py
@@ -5,40 +5,41 @@
 ## Help from: https://stackoverflow.com/questions/930397/getting-the-last-element-of-a-list-in-python << Getting last item in list in Python
 
 flag = False
-num = 169
+num = 468
 numList = []
 primeList = []
 
 def findLowerPrime(num):
-    print("Find lower prime: ")
+    print("Find lower prime ...")
     for i in range(2,num+1):
         if (i==2):
             numList.append(i)
             primeList.append(i)
         else:
             for x in numList:
-                if(i % x == 0):
-                    break
+                if(x == numList[-1]):
+                    primeList.append(i)
                 else:
-                    if(x == len(numList)+1):
-                        print(str(i) + " is a prime")
-                        primeList.append(i)
+                    if(i % x == 0):
+                        break
             numList.append(i)
 
-def findUpperPrime(num,flag):
-    print("Find upper prime")
-    while(flag == False):
-        numList.append(num+1)
-        for i in range(2,len(numList)+1):
-            for x in numList:
-                if(i % x != 0):
-                    if(x == len(numList)+1):
-                        print(str(i) + " is a prime")
-                        primeList.append(i)
-                        flag = True
-
-
+def findUpperPrime(flag):
+    print("Finding upper prime ...")
+    while (flag == False):
+        numList.append(numList[-1]+1)
+        for x in numList:
+            if(x == numList[-1]):
+                primeList.append(numList[-1])
+                flag = True
+                break
+            else:
+                if (numList[-1] % x == 0):
+                    break
 
 findLowerPrime(num)
-print(str(primeList[-1]))
-findUpperPrime(num,flag)
+findUpperPrime(flag)
+if(num in primeList):
+    print(str(num) + " is a prime number!")
+else:
+    print(str(primeList[-2]) + " < " + str(num) + " < " + str(primeList[-1]))
