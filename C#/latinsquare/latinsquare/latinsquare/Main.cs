a95bf7e6a3e33166ebe4e7c3832894f753ef9283
mblaul <32026169+mblaul@users.noreply.github.com>
2018-01-08 22:36:03 -0500

    Latin Square Determination

---
+++ C#/latinsquare/latinsquare/latinsquare/Main.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace latinsquare
+{
+    public partial class Main : Form
+    {
+        public Main()
+        {
+            InitializeComponent();
+            IsLatinSquare();
+        }
+
+        public void IsLatinSquare()
+        {
+            int length = 5;
+            string[] input = "1 2 3 4 5 5 1 2 3 4 4 5 1 2 3 3 4 5 1 2 2 3 4 5 1".Split();
+            int[] intput = Array.ConvertAll(input, s => int.Parse(s));
+
+
+
+
+            Console.Out.Write(intput[2]);
+        }
+    }
+}
